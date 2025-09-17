7b9783265b54f8b2e3beaf8da0589a7c610cbaa7
kyoungjooo <leekjoo1008@gmail.com>
2025-09-17 16:42:50 +0900

    feat: KakaoProvider

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -1,14 +1,12 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import KakaoProvider from "next-auth/providers/kakao";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
 };
 
